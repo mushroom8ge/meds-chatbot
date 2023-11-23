c88b4385ca75d0d8681ea8c09c6e881bc6dabea7
Tongtong Jia <tongtong.jia@orionhealth.com>
2023-11-23 13:49:40 +1100

    initial commit

---
+++ my-app/src/landingpage1.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { Switch, NavBar, Checkbox, Radio, Input } from "antd-mobile";
+import './common.css';
+
+class LandingPage extends Component {
+  state = {};
+
+  setValue(val) {}
+
+  render() {
+    return (
+        <div className="page flex-col">
+        <div className="group_1 flex-row justify-between">
+          <div className="group_2 flex-col">
+            <div className="text-wrapper_1 flex-col">
+              <span className="text_1">new&nbsp;chat</span>
+            </div>
+            <img
+              className="label_1"
+              src={"https://lanhu.oss-cn-beijing.aliyuncs.com/MasterDDSSlicePNG7058f070968149bce9e2b90b6257511e.png"}
+            />
+            <span className="paragraph_1">
+              <br />
+              <br />
+              <br />
+              Welcome&nbsp;to&nbsp;use&nbsp;chatbot
+              <br />
+              <br />
+              some&nbsp;guide&nbsp;info&nbsp;about&nbsp;the&nbsp;robot&nbsp;
+              <br />
+              <br />
+              balalala&nbsp;&nbsp;balalala
+              <br />
+              <br />
+              balalala
+            </span>
+          </div>
+          <div className="dialog_box flex-col">
+            <img
+              className="image_1"
+              src={"https://lanhu.oss-cn-beijing.aliyuncs.com/MasterDDSSlicePNG0b77848fc2b7d87be3cf9ceb67ab7993.png"}
+            />
+          </div>
+        </div>
+        <div className="group_3 flex-row justify-between">
+          <img
+            className="oh_logo"
+            src={"https://lanhu.oss-cn-beijing.aliyuncs.com/MasterDDSSlicePNG1f1c91c83883625a3eff3fd6e741f254.png"}
+          />
+          <div className="block_1 flex-col">
+            <div className="group_4 flex-col">
+              <span className="text_2">text</span>
+              <div className="text-wrapper_2 flex-col">
+                <span className="text_3">Send&nbsp;&nbsp;a&nbsp;message</span>
+                {/* <Input
+                  placeholder="Type your message here"
+                    // Add any other props or event handlers as needed  
+                /> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
---
+++ rasa_dev/actions/actions.py
@@ -0,0 +1,348 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+import rasa_sdk
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+from rasa_sdk.events import SlotSet, FollowupAction
+from rasa_sdk.forms import FormValidationAction
+import requests
+import base64
+from datetime import datetime as dt
+import pytz
+
+# We use the medicare.gov database to find information about 3 different
+# healthcare facility types, given a city name, zip code or facility ID
+# the identifiers for each facility type is given by the medicare database
+# xubh-q36u is for hospitals
+# b27b-2uc7 is for nursing homes
+# 9wzi-peqs is for home health agencies
+ENDPOINTS = {
+    "base": "https://data.medicare.gov/resource/{}.json",
+    "xubh-q36u": {
+        "city_query": "?city={}",
+        "zip_code_query": "?zip_code={}",
+        "id_query": "?provider_id={}"
+    },
+    "b27b-2uc7": {
+        "city_query": "?provider_city={}",
+        "zip_code_query": "?provider_zip_code={}",
+        "id_query": "?federal_provider_number={}"
+    },
+    "9wzi-peqs": {
+        "city_query": "?city={}",
+        "zip_code_query": "?zip={}",
+        "id_query": "?provider_number={}"
+    }
+}
+
+ENDPOINT = {"status": "http://medicines-nightly-dmd.orion.internal:19080/fhirng/4.0/List?patient.identifier={}&_format-json&code=medication-history&status={}",
+            "base": "http://medicines-nightly-dmd.orion.internal:19080/fhirng/4.0/List?patient.identifier={}&_format-json&code=medication-history"}
+
+USERNAME = "clinician"
+PASSWORD = "b"
+HEADERS = {
+    "Authorization": "Basic " + base64.b64encode(f"{USERNAME}:{PASSWORD}".encode()).decode()
+}
+
+FACILITY_TYPES = {
+    "hospital":
+        {
+            "name": "hospital",
+            "resource": "xubh-q36u"
+        },
+    "nursing_home":
+        {
+            "name": "nursing home",
+            "resource": "b27b-2uc7"
+        },
+    "home_health":
+        {
+            "name": "home health agency",
+            "resource": "9wzi-peqs"
+        }
+}
+
+def _create_viewer_path(patient_id: Text, medication_status: Text) -> Text:
+    """Creates a path to a specific patient's medicine history."""
+    if medication_status in ["preparation", "in-progress", "cancelled", "on-hold", "completed", "entered-in-error","stopped", "declined", "unknown", "active"]:
+        return ENDPOINT["status"].format(patient_id, medication_status)
+    return ENDPOINT["base"].format(patient_id)
+
+def _create_path(base: Text, resource: Text,
+                 query: Text, values: Text) -> Text:
+    """Creates a path to find provider using the endpoints."""
+
+    if isinstance(values, list):
+        return (base + query).format(
+            resource, ', '.join('"{0}"'.format(w) for w in values))
+    else:
+        return (base + query).format(resource, values)
+
+def _find_facilities(location: Text, resource: Text) -> List[Dict]:
+    """Returns json of facilities matching the search criteria."""
+
+    if str.isdigit(location):
+        full_path = _create_path(ENDPOINTS["base"], resource,
+                                 ENDPOINTS[resource]["zip_code_query"],
+                                 location)
+    else:
+        full_path = _create_path(ENDPOINTS["base"], resource,
+                                 ENDPOINTS[resource]["city_query"],
+                                 location.upper())
+    #print("Full path:")
+    #print(full_path)
+    results = requests.get(full_path).json()
+    return results
+
+
+def _resolve_name(facility_types, resource) ->Text:
+    for key, value in facility_types.items():
+        if value.get("resource") == resource:
+            return value.get("name")
+    return ""
+
+class FindFacilityTypes(Action):
+    """This action class allows to display buttons for each facility type
+    for the user to chose from to fill the facility_type entity slot."""
+
+    def name(self) -> Text:
+        """Unique identifier of the action"""
+
+        return "find_facility_types"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List:
+
+        buttons = []
+        for t in FACILITY_TYPES:
+            facility_type = FACILITY_TYPES[t]
+            payload = "/inform{\"facility_type\": \"" + facility_type.get(
+                "resource") + "\"}"
+
+            buttons.append(
+                {"title": "{}".format(facility_type.get("name").title()),
+                 "payload": payload})
+
+        # TODO: update rasa core version for configurable `button_type`
+        dispatcher.utter_button_template("utter_greet", buttons, tracker)
+        return []
+
+
+class FindHealthCareAddress(Action):
+    """This action class retrieves the address of the user's
+    healthcare facility choice to display it to the user."""
+
+    def name(self) -> Text:
+        """Unique identifier of the action"""
+
+        return "find_healthcare_address"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict]:
+
+        facility_type = tracker.get_slot("facility_type")
+        healthcare_id = tracker.get_slot("facility_id")
+        full_path = _create_path(ENDPOINTS["base"], facility_type,
+                                 ENDPOINTS[facility_type]["id_query"],
+                                 healthcare_id)
+        results = requests.get(full_path).json()
+        if results:
+            selected = results[0]
+            if facility_type == FACILITY_TYPES["hospital"]["resource"]:
+                address = "{}, {}, {} {}".format(selected["address"].title(),
+                                                 selected["city"].title(),
+                                                 selected["state"].upper(),
+                                                 selected["zip_code"].title())
+            elif facility_type == FACILITY_TYPES["nursing_home"]["resource"]:
+                address = "{}, {}, {} {}".format(selected["provider_address"].title(),
+                                                 selected["provider_city"].title(),
+                                                 selected["provider_state"].upper(),
+                                                 selected["provider_zip_code"].title())
+            else:
+                address = "{}, {}, {} {}".format(selected["address"].title(),
+                                                 selected["city"].title(),
+                                                 selected["state"].upper(),
+                                                 selected["zip"].title())
+
+            return [SlotSet("facility_address", address)]
+        else:
+            print("No address found. Most likely this action was executed "
+                  "before the user choose a healthcare facility from the "
+                  "provided list. "
+                  "If this is a common problem in your dialogue flow,"
+                  "using a form instead for this action might be appropriate.")
+
+            return [SlotSet("facility_address", "not found")]
+
+
+class FacilityForm(FormValidationAction):
+    """Custom form action to fill all slots required to find specific type
+    of healthcare facilities in a certain city or zip code."""
+
+    def name(self) -> Text:
+        """Unique identifier of the form"""
+
+        return "facility_form"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+
+        return ["facility_type", "location"]
+
+    def slot_mappings(self) -> Dict[Text, Any]:
+        return {"facility_type": self.from_entity(entity="facility_type",
+                                                  intent=["inform",
+                                                          "search_provider"]),
+                "location": self.from_entity(entity="location",
+                                             intent=["inform",
+                                                     "search_provider"])}
+
+    def submit(self,
+               dispatcher: CollectingDispatcher,
+               tracker: Tracker,
+               domain: Dict[Text, Any]
+               ) -> List[Dict]:
+        """Once required slots are filled, print buttons for found facilities"""
+
+        location = tracker.get_slot('location')
+        facility_type = tracker.get_slot('facility_type')
+
+        results = _find_facilities(location, facility_type)
+        button_name = _resolve_name(FACILITY_TYPES, facility_type)
+        if len(results) == 0:
+            dispatcher.utter_message(
+                "Sorry, we could not find a {} in {}.".format(button_name,
+                                                              location.title()))
+            return []
+
+        buttons = []
+        # limit number of results to 3 for clear presentation purposes
+        for r in results[:3]:
+            if facility_type == FACILITY_TYPES["hospital"]["resource"]:
+                facility_id = r.get("provider_id")
+                name = r["hospital_name"]
+            elif facility_type == FACILITY_TYPES["nursing_home"]["resource"]:
+                facility_id = r["federal_provider_number"]
+                name = r["provider_name"]
+            else:
+                facility_id = r["provider_number"]
+                name = r["provider_name"]
+
+            payload = "/inform{\"facility_id\":\"" + facility_id + "\"}"
+            buttons.append(
+                {"title": "{}".format(name.title()), "payload": payload})
+
+        if len(buttons) == 1:
+            message = "Here is a {} near you:".format(button_name)
+        else:
+            if button_name == "home health agency":
+                button_name = "home health agencie"
+            message = "Here are {} {}s near you:".format(len(buttons),
+                                                         button_name)
+
+        # TODO: update rasa core version for configurable `button_type`
+        dispatcher.utter_button_message(message, buttons)
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+
+class ActionFindMedication(Action):
+    """This action class retrieves the medication of the user in specific status
+     to display it to the user."""
+
+    def name(self) -> Text:
+        """Unique identifier of the action"""
+
+        return "action_find_medication"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict]:
+
+        patient_id = tracker.get_slot("patient_id")
+        medication_status = tracker.get_slot("medication_status") # TODO: status should be standardised
+        time = next(tracker.get_latest_entity_values("time"), None)
+        print("this is the time your input!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(time)
+        
+        
+        print(type(time))
+        date_start = dt.min.replace(tzinfo=pytz.utc)
+        date_end = dt.max.replace(tzinfo=pytz.utc)
+        if time:
+            if time['from']:
+                date_start = dt.strptime(time['from'], "%Y-%m-%dT%H:%M:%S.%f%z")
+                print(date_start.tzinfo)
+            if time['to']:
+                date_end = dt.strptime(time['to'], "%Y-%m-%dT%H:%M:%S.%f%z")
+
+        # assume the patient's id and status is in the range now
+        dispatcher.utter_message("medication_status is {}".format(medication_status))
+        dispatcher.utter_message("patient_id is {}".format(patient_id))
+
+        full_path = _create_viewer_path(patient_id, medication_status) 
+        dispatcher.utter_message(full_path)
+        # results = requests.get(full_path, headers = HEADERS).json()
+        response = requests.get(full_path, headers = HEADERS)
+        dispatcher.utter_message("request code: {}".format(response.status_code))
+        results = response.json()
+
+        if results: 
+            medications = []
+            entries = results.get("entry")
+
+            if entries:
+                for entry in entries:
+                    resource = entry.get("resource")
+                    if resource and "status" in resource:
+                        if medication_status=="all" or medication_status in resource.get("status"):
+
+                            if time:
+                                effective_datetime = resource.get("effectiveDateTime")
+                                if not effective_datetime:
+                                    effective_datetime = resource.get("dateAsserted")
+                                    if not effective_datetime:
+                                        continue
+                                    date = dt.strptime(effective_datetime, "%Y-%m-%dT%H:%M:%S%z")
+                                    if date >= date_start and date <= date_end:
+                                        
+                                        codings= resource.get("medicationCodeableConcept").get("coding")
+                                        for coding in codings:
+                                            medications.append(coding.get("display"))
+
+                            else: # users don't care about time
+                                codings= resource.get("medicationCodeableConcept").get("coding")
+                                for coding in codings:
+                                    medications.append(coding.get("display"))
+                            
+            if len(medications) > 0:
+                print("here are the medications you may want to check!")
+                print(medications)
+                return [SlotSet("medications", medications)]
+        print("No medications found.")
+        return [SlotSet("medications", ["not found"])]
